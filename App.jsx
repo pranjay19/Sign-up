ed5327880e39f9d63f36d2a5a5b9c9c281674e1c
PRANJAY GULERIA <74106002+pranjay19@users.noreply.github.com>
2023-03-24 11:22:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+
+import "./App.css"
+
+
+
+
+
+function App(){
+
+ 
+     
+    return(
+        <>
+       <div class="login-box">
+  <h2>Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required=""></input>
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required=""></input>
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div>
+  </>
+
+  
+
+    );
+}
+
+export default App;
